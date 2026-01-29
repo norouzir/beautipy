464fe174de12b99de296484c0c76ae325a904009
sLOW <mohamadreza526@gmail.com>
2026-01-29 17:19:17 +0330

    refactor: replace write-and-delete logic with predictive formatting

--- beautipy.py
+++ beautipy.py
@@ -4,12 +4,12 @@ def format_structure(
     opener_in_next_line: bool = True,
     add_space_to_assignments: bool = True,
     expand_empty_objects: bool = False,
-    indent: str = '    '
+    indent_string: str = '    '
 ) -> str:
 
     def newline(count=0):
         count = count if count else 2 if indent_level<extra_line_depth else 1
-        return count * ('\n' + indent_level * indent)
+        return count * ('\n' + indent_level * indent_string)
 
     if extra_line_depth < 0:
         raise ValueError('extra_line_depth must be equal or greater than 0')
@@ -19,20 +19,20 @@ def format_structure(
     QUOTES = {"'", '"'}
     indent_level = 0
     string_opener = None
-    last_was_newline = False
     last_was_escape = False
     buffer = []
     source_text = source if isinstance(source, str) else str(source)
-
-    for char in source_text:
+    i = -1
+    
+    while i < len(source_text)-1:
+        i += 1
+        char = source_text[i]
 
         if string_opener:
             buffer.append(char)
-
             if last_was_escape:
                 last_was_escape = False
                 continue
-        
             if char == '\\':
                 last_was_escape = True
             elif char == string_opener:
@@ -43,52 +43,46 @@ def format_structure(
             continue
 
         if char in OPENERS:
-            if opener_in_next_line and buffer and not last_was_newline:
-                buffer.append(newline(1))
-            indent_level += 1
-            buffer.extend((char, newline()))
-            last_was_newline = True
+            stripped = source_text[i+1:].strip()
+            if stripped and stripped[0] in CLOSERS:
+                if expand_empty_objects:
+                    if opener_in_next_line and buffer and not buffer[-1].endswith(indent_string):
+                        buffer.append(newline(1))
+                    buffer.append(char + newline() + stripped[0])
+                else:
+                    buffer.append(char + stripped[0])
+                i += source_text[i+1:].find(stripped[0]) + 1
+            else:
+                if opener_in_next_line and buffer and not buffer[-1].endswith(indent_string):
+                    buffer.append(newline(1))
+                indent_level += 1
+                buffer.append((char + newline()))
             continue
             
         if char == ',':
             buffer.append(char + newline())
-            last_was_newline = True
             continue
         
         if char in CLOSERS:
-            if last_was_newline:
-                del buffer[-1]
-                if expand_empty_objects:
-                    indent_level = max(indent_level - 1, 0)
-                    buffer.append(newline())
-                else:
-                    del buffer[-2]
-                    indent_level = max(indent_level - 1, 0)
-            else:
-                buffer.append(newline())
-                buffer[-1] = buffer[-1].removesuffix(indent)
-                indent_level = max(indent_level - 1, 0)
+            buffer.append(newline())
+            buffer[-1] = buffer[-1].removesuffix(indent_string)
+            indent_level = max(indent_level - 1, 0)
             buffer.append(char)
-            last_was_newline = False
             continue
         
         if char == '=' and add_space_to_assignments:
             buffer.append(' = ')
-            last_was_newline = False
             continue
         
         if char == ':' and add_space_to_assignments:
             buffer.append(': ')
-            last_was_newline = False
             continue
         
         if char in QUOTES:
             string_opener = char
             buffer.append(char)
-            last_was_newline = False
             continue
 
         buffer.append(char)
-        last_was_newline = False
     
     return ''.join(buffer)
